77cf4cc82a192882bd6b3af6abebcbb8636aeabc
eri <89553887+erikoleci@users.noreply.github.com>
2025-02-24 18:43:51 +0000

    debug off

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ logger = logging.getLogger(__name__)
 if __name__ == "__main__":
     try:
         logger.info("Starting Flask server on port 5000...z")
-        app.run(host="0.0.0.0", port=5000, debug=True)
+        app.run(host="0.0.0.0", port=5000)
     except Exception as e:
         logger.error(f"Failed to start server: {str(e)}")
         raise
